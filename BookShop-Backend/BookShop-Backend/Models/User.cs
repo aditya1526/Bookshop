9e7eebfc56bcb297b11c40ea19b682d733275656
aditya1526 <126767412+aditya1526@users.noreply.github.com>
2023-03-02 15:06:01 +0530

    Base

---
+++ BookShop-Backend/BookShop-Backend/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop_Backend.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+        public String Mobile { get; set; }
+        public String Gender { get; set; }
+        public String Pwd { get; set; }
+        public DateTime MemberSince { get; set; }
+
+
+
+    }
+}
